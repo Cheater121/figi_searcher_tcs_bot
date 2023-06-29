a02a16976297eec6b7a3f0c28bbfc493262d2c20
Cheater121 <106826455+Cheater121@users.noreply.github.com>
2023-06-29 14:26:37 +0300

    Upload config.py

--- config_data/config.py
+++ config_data/config.py
@@ -1,1 +1,27 @@
+from dataclasses import dataclass
+from environs import Env
 
+
+@dataclass
+class TgBot:
+    token: str
+
+
+@dataclass
+class TCSClient:
+    token: str   
+
+
+@dataclass
+class Config:
+    tcs_client: TCSClient
+    tg_bot: TgBot
+
+
+def load_config(path: str = None) -> Config:
+    env: Env = Env()
+    env.read_env(path)
+
+    return Config(tg_bot=TgBot(token=env('FIGI_BOT_TOKEN')),
+                  tcs_client=TCSClient(token=env('TCS_TOKEN')))
+                                    
